da885ee262d590414a119956f87b0ee3466a2184
Aleksandra Ivanova <ival96vl@gmail.com>
2026-06-09 20:53:54 +0300

    Added negative tests

--- src/test/java/tests/RegistrationFormTests.java
+++ src/test/java/tests/RegistrationFormTests.java
@@ -4,14 +4,16 @@ import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
-import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.selector.ByDeepShadow.cssSelector;
 import static java.nio.channels.Selector.open;
 
 public class RegistrationFormTests extends TestBase {
 
     @Test
-    void successfulFillFormTest () {
+    void submitFullFormTest () {
         Selenide.open ("/automation-practice-form");
         $("#firstName").val("Александра");
         $("#lastName").val("Иванова");
@@ -44,11 +46,72 @@ public class RegistrationFormTests extends TestBase {
         $(".table-responsive").shouldHave(text("State and City Haryana Panipat"));
         $("#closeLargeModal").click();
 
-
-
-
-
-
     }
+
+    @Test
+    void submitRequiredFieldsOnlyTest() {
+        Selenide.open("/automation-practice-form");
+
+        $("#firstName").val("Александра");
+        $("#lastName").val("Иванова");
+        $("#userEmail").val("aleks@ivanova.com");
+        $("#gender-radio-2").selectRadio("Female");
+        $("#userNumber").val("7123456789");
+        $("#submit").click();
+        $(".modal-title").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text("Student Name Александра Иванова"));
+        $(".table-responsive").shouldHave(text("Student Email aleks@ivanova.com"));
+        $(".table-responsive").shouldHave(text("Gender Female"));
+        $(".table-responsive").shouldHave(text("Mobile 7123456789"));
+    }
+
+    @Test
+    void submitEmptyFormTest() {
+        Selenide.open("/automation-practice-form");
+
+        $("#submit").click();
+        $(".modal-title").shouldNotBe(visible);
+        $("#firstName:invalid").shouldBe(visible);
+    }
+
+    @Test
+    void submitInvalidEmailTest() {
+        Selenide.open("/automation-practice-form");
+
+        $("#firstName").val("Александра");
+        $("#lastName").val("Иванова");
+        $("#genterWrapper").$(byText("Female")).click();
+        $("#userNumber").val("7123456789");
+        $("#userEmail").val("://ivanova.com");
+        $("#submit").click();
+        $(".modal-title").shouldNotBe(visible);
+        $("#userEmail:invalid").shouldBe(visible);
+    }
+
+    @Test
+    void submitTooShortMobileNumberTest() {
+        Selenide.open("/automation-practice-form");
+
+        $("#firstName").val("Александра");
+        $("#lastName").val("Иванова");
+        $("#genterWrapper").$(byText("Female")).click();
+        $("#userNumber").val("712345678");
+        $("#submit").click();
+        $(".modal-title").shouldNotBe(visible);
+        $("#userNumber:invalid").shouldBe(visible);
+    }
+
+    @Test
+    void submitMissingRequiredLastNameTest() {
+        Selenide.open("/automation-practice-form");
+
+        $("#firstName").val("Александра");
+        $("#genterWrapper").$(byText("Female")).click();
+        $("#userNumber").val("7123456789");
+        $("#submit").click();
+        $(".modal-title").shouldNotBe(visible);
+        $("#lastName:Invalid").shouldBe(visible);
+    }
+
 
 }
